481d1bdfcaf882253917fad0b76cb0182fe368a8
raphael008 <raphael008@icloud.com>
2020-09-11 21:49:20 +0800

    clean code

--- Answer216.java
+++ Answer216.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Answer216 {
     public static List<Integer> temp = new ArrayList<>();
